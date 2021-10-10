548d3ca66558283899581eace5e7af51de48eb1b
ma-camp101 <cam20032@byui.edu>
2021-10-10 18:09:56 -0400

    changes

--- lesson3/js/dates.js
+++ lesson3/js/dates.js
@@ -1,2 +1,1 @@
 document.querySelector("#year").textContent = new Date().getFullYear();
-document.querySelector("#modified").textContent = `Last Updated: ${document.lastModified}`;
--- lesson4/js/dates.js
+++ lesson4/js/dates.js
@@ -1,2 +1,4 @@
 document.querySelector("#year").textContent = new Date().getFullYear();
-const a = new Date;
+const today = new Date();
+const options = { weekday: 'long', day: 'short', month: 'medium', year: 'numeric' };
+document.querySelector("#current").textContent = new Date(today.toLocaleDateString('en-UK', options));
