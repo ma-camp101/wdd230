c5d3c5932053219af57e150976003ac8cc9cae48
ma-camp101 <cam20032@byui.edu>
2021-10-13 01:36:05 -0400

    changes

--- lesson3/js/menu.js
+++ lesson3/js/menu.js
@@ -1,3 +1,7 @@
 function switchMenu() {
     document.getElementsByClassName("button")[0].classList.toggle("switch");
 }
+
+document.getElementsByClassName("button").addEventListener("click", function() {
+  if (this.classList.contains("switch")) {this.classList.remove("switch");} 
+  else this.classList.add("switch");});
