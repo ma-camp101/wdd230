f68d5647da301b828ec4509c0225e1a9dbb69f0e
ma-camp101 <cam20032@byui.edu>
2021-10-17 01:22:38 -0400

    changes

---
+++ lesson5/js2/event.js
@@ -0,0 +1,11 @@
+let day = getDay();
+let event = document.querySelector("event")
+function event() {
+    if(day == 5) {
+        day.classList.add("show");
+    }
+    else { 
+        event;
+    }
+     
+}
