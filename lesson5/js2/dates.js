e1755f442d5227168166d7634802bbe9ab1157f6
ma-camp101 <cam20032@byui.edu>
2021-10-19 23:55:29 -0400

    changes

--- lesson5/js2/dates.js
+++ lesson5/js2/dates.js
@@ -1,4 +1,7 @@
 document.querySelector("#year").textContent = new Date().getFullYear();
-let today = new Date();
-let options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' };
-document.getElementById("current").innerHTML = today.toLocaleDateString('en-GB', options);
+function currentDate() {
+    let today = new Date();
+    let options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' };
+    document.getElementById("current").innerHTML = today.toLocaleDateString('en-GB', options);  
+}
+currentDate();
