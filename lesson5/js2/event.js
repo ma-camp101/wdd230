7f3215ad1e47fee42bc821b13d4070445a81fd39
ma-camp101 <cam20032@byui.edu>
2021-10-20 23:11:16 -0400

    changes

--- lesson5/js2/event.js
+++ lesson5/js2/event.js
@@ -1,11 +1,11 @@
 let today = new Date()
 let day = today.getDay();
 function event() {
-    if(day == 2) {
+    if(day == 3) {
         friday = "Saturday: Preston Pancakes in the Park! 9 a.m. Saturday at the City Park.";
     }
     else { 
         friday = "";
     }
-    document.querySelector(".event").innerHTML = friday;
+    document.getElementById("event").innerHTML = friday;
 }
