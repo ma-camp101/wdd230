ed87aa7928eda3f54da3025582cb70f035e2a4a6
ma-camp101 <cam20032@byui.edu>
2021-10-21 18:49:53 -0400

    changes

--- lesson5/js2/event.js
+++ lesson5/js2/event.js
@@ -1,8 +1,8 @@
 let today = new Date()
 let day = today.getDay();
 function event() {
-    if(day == 3) {
-        friday = "Saturday: Preston Pancakes in the Park! 9 a.m. Saturday at the City Park.";
+    if(day == 4) {
+        friday = ;
     }
     else { 
         friday = "";
