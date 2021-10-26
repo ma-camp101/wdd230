28b4df054c468cf84bf76959aa733622a7c1eee7
ma-camp101 <cam20032@byui.edu>
2021-10-26 15:49:37 -0400

    changes

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,7 +1,7 @@
-function doInputOutput() {
+function windChill() {
     var tempF = parseInt(document.getElementById('temp').value);
     var speed = parseInt(document.getElementById('wind').value);
-    var chill = windChill(tempF, speed);
+    var chill = calculateChill(tempF, speed);
     if ((tempF <= 50) && (speed >= 4.8)){
         document.getElementById('output').innerHTML = chill + "\u00B0F";
     }
@@ -9,7 +9,7 @@ function doInputOutput() {
         document.getElementById('output').innerHTML = "N/A";
     }
 }
-function windChill(tempF, speed) {
+function calculateChill(tempF, speed) {
     f = 35.74 + 0.6215 * tempF - 35.75 * Math.pow(speed, 0.16) + 0.4275 * tempF * Math.pow(speed, 0.16);
     return f.toFixed(1);
 }
