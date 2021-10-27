a8d52dda4b9680fa9e204003170e9a036e2bec82
ma-camp101 <cam20032@byui.edu>
2021-10-26 20:56:43 -0400

    changes

--- lesson6/js/navigation.js
+++ lesson6/js/navigation.js
@@ -1,3 +1,5 @@
+// let hidemenu = document.querySelector(".menu li a:first-child");
+// hidemenu.addEventListener("onclick", switchMenu());
 function switchMenu() {
     let menu = document.querySelector(".menu");
     menu.classList.toggle("hide");
