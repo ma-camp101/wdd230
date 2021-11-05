07e6f06bdc91d7abeb88d1faa959951fc2909e3d
ma-camp101 <cam20032@byui.edu>
2021-11-05 17:31:31 -0400

    changes

--- lesson8/js/event.js
+++ lesson8/js/event.js
@@ -8,16 +8,3 @@ function eventShow() {
         document.getElementById("special").classList.remove("reveal");
     }
 }
-eventShow();
-// const today = new Date(); 
-// const weekday = parseInt(today.getDay());  
-// function hideMessage() {     
-//     if (weekday == 5) {        
-//         document.getElementById('fridayMessage').classList.remove("hide");
-//     }     
-//     else {
-//         document.getElementById('fridayMessage').classList.add("hide");
-//     }
-// }
-
-// document.getElementById('weekday').textContent = weekday;
