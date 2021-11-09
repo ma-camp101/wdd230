00ff9d6e1a40199c849b39b6f40d5eeb86e8147a
ma-camp101 <cam20032@byui.edu>
2021-11-09 17:45:08 -0500

    changes

--- lesson7/js/daysbetween.js
+++ lesson7/js/daysbetween.js
@@ -4,11 +4,11 @@ prior = document.querySelector(".lastVisit");
 prior.addEventListener("onload", lastVisit());
 function lastVisit() {
     if (priorVisit != '') {
-        visit = ((priorVisit - Date.now()) / millisecondsToDays).toFixed(0);
+        visit = ((priorVisit - today) / millisecondsToDays).toFixed(0);
     }
     else {
-        visit = Date.now() + " This is your first visit. Welcome!"
+        visit = today + " This is your first visit. Welcome!"
     }
     document.getElementById("lastv").innerHTML = visit;
-    localStorage.setItem('lastvisit', Date.now());
+    localStorage.setItem('lastvisit', today);
 }
---
+++ lesson9/js/event.js
@@ -0,0 +1,6 @@
+let day = today.getDay();
+let saturdayMsg = document.querySelector(".special");
+saturdayMsg.addEventListener("onload", eventShow());
+function eventShow() {
+    if(day == 5) { saturdayMsg.classList.add("reveal"); }
+    else { saturdayMsg.classList.remove("reveal"); }}
