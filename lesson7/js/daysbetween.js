e755f5102e4a7b7cec7bcdf696bdb01257db533c
ma-camp101 <cam20032@byui.edu>
2021-11-11 15:37:24 -0500

    changes

--- lesson7/js/daysbetween.js
+++ lesson7/js/daysbetween.js
@@ -4,12 +4,12 @@ function lastVisit() {
     let priorVisit = localStorage.getItem('lastvisit');
     let obj = new Date(priorVisit);
     if (priorVisit != '') {
-        visit = ((today - obj) / millisecondsToDays).toFixed(2);
+        visit = ((today - obj) / millisecondsToDays).toFixed(0);
     }
     else {
         visit = today + " This is your first visit. Welcome!"
     }
-    document.getElementById("lastv").innerHTML = visit;
+    document.getElementById("lastv").innerHTML = visit + " days ago. Welcome!";
     localStorage.setItem('lastvisit', today);
 }
 lastVisit();
--- lesson7/lazy-load-js/lazy-load.js
+++ lesson7/lazy-load-js/lazy-load.js
@@ -1,12 +1,12 @@
 let imagesToLoad = document.querySelectorAll('img[data-src]');
-const loadImages = (image) => {
+let loadImages = (image) => {
   image.setAttribute('src', image.getAttribute('data-src'));
   image.onload = () => {
     image.removeAttribute('data-src');
   };
 };
 if('IntersectionObserver' in window) {
-    const observer = new IntersectionObserver((items, observer) => {
+    let observer = new IntersectionObserver((items, observer) => {
       items.forEach((item) => {
         if(item.isIntersecting) {
           loadImages(item.target);
