42b5d0df8a85cae4c58ab2b05a4e0c8ab355e128
ma-camp101 <cam20032@byui.edu>
2021-11-11 18:24:25 -0500

    changes

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -1,21 +1,19 @@
 let requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
-let prophets = jsonObject['prophets'];
 fetch(requestURL)
   .then(function (response) {
     return response.json();
   })
   .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
-    for (let i = 0; i < prophets.length; i++ ) {
-        let card = document.createElement('section');
-        let h2 = document.createElement('h2');
-    
-    h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-    
+    // console.table(jsonObject);  // temporary checking for valid response and data parsing
+    let prophets = jsonObject['prophets'];
+    prophets.forEach((prophet) => {
+      let card = document.createElement('section');
+      let h2 = document.createElement('h2');
+      h2.innerHTML = 
+        prophet.name + ' <spar style="color:navy">' + prophet.lastname + '</spar>';
     card.appendChild(h2);
-    }
     document.querySelector('div.cards').appendChild(card);
-    image.setAttribute('src', prophets[i].imageurl);
+    });
   });
   
  
