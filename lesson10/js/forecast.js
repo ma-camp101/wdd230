907885d7f49cfe2a572e3ee10a78f5b828a98273
ma-camp101 <cam20032@byui.edu>
2021-11-17 00:23:03 -0500

    changes

--- lesson10/js/forecast.js
+++ lesson10/js/forecast.js
@@ -9,6 +9,9 @@ fetch(apiURL)
   .then((jsObject) => {
     console.log(jsObject)
     windChill(jsObject)
-    document.getElementById('temp').innerHTML = `${jsObject.main.temp} &#176;F`;
-    document.getElementById('')
+    document.getElementById('weatherdesc').innerHTML = `${jsObject.weather["description"]}`;
+    document.getElementById('temp').innerHTML = `${jsObject.main.temp}&#176;F`;
+    document.getElementById('hightemp').innerHTML = `${jsObject.main.temp_max}&#176;F`;
+    document.getElementById('humidity').innerHTML = `${jsObject.main.humidity}%`;
+    document.getElementById('wind').innerHTML = `${jsObject.wind.speed}mph`
 });
