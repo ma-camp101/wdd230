912ed24f7e78790926796f84644af91c237bc0ff
ma-camp101 <cam20032@byui.edu>
2021-11-18 19:52:22 -0500

    changes

--- lesson10/js/forecast.js
+++ lesson10/js/forecast.js
@@ -10,7 +10,6 @@ fetch(apiURL)
     windChill(jsObject)
     const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';
     const desc = jsObject.weather[0].description; 
-    const sixpm = jsObject.list.filter(x => x.dt_txt.includes('18:00:00'));
     document.getElementById('currenticon').setAttribute('src', imagesrc);
     document.getElementById('currenticon').setAttribute('alt', desc);
     document.getElementById('weatherdesc').innerHTML = `${jsObject.weather[0].description}`;
@@ -18,11 +17,14 @@ fetch(apiURL)
     document.getElementById('hightemp').innerHTML = `${jsObject.main.temp_max.toFixed(1)}&#176;F`;
     document.getElementById('humidity').innerHTML = `${jsObject.main.humidity}%`;
     document.getElementById('wind').innerHTML = `${jsObject.wind.speed.toFixed(1)}mph`;
+    const sixpm = jsObject.list.filter(x => x.dt_txt.includes('18:00:00'));
     sixpm.forEach(forecast => {
       let day = 0;
       let thedate = new Date(forecast.dt_txt);
       const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
       document.querySelector(`#dayofweek${day + 1}`).innerHTML = weekdays[thedate.getDay()];
+      document.querySelector(`#icon${day + 1}`).setAttribute('src', imagesrc);
+      document.querySelector(`#icon${day + 1}`).setAttribute('alt', desc);
       document.querySelector(`#forecast${day + 1}`).innerHTML = forecast.main.temp;
       day++
     }); 
