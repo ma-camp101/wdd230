858c4c9463bf3cf1cd6a27b5d9d7aea4538fd450
ma-camp101 <cam20032@byui.edu>
2021-11-18 19:01:54 -0500

    changes

--- lesson10/js/forecast.js
+++ lesson10/js/forecast.js
@@ -10,32 +10,20 @@ fetch(apiURL)
     windChill(jsObject)
     const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';
     const desc = jsObject.weather[0].description; 
+    const sixpm = jsObject.list.filter(x => x.dt_txt.includes('18:00:00'));
     document.getElementById('currenticon').setAttribute('src', imagesrc);
     document.getElementById('currenticon').setAttribute('alt', desc);
     document.getElementById('weatherdesc').innerHTML = `${jsObject.weather[0].description}`;
     document.getElementById('temp').innerHTML = `${jsObject.main.temp.toFixed(1)}&#176;F`;
     document.getElementById('hightemp').innerHTML = `${jsObject.main.temp_max.toFixed(1)}&#176;F`;
     document.getElementById('humidity').innerHTML = `${jsObject.main.humidity}%`;
-    document.getElementById('wind').innerHTML = `${jsObject.wind.speed.toFixed(1)}mph`
-  });
-fetch(apiURL)
-  .then((response) => {
-    if (!response.ok) {
-      throw Error(response.statusText);
-    } else {
-    return response.json();
-    }})
-  .then((fiveDay) => {
-    const sixpm = fiveDay.list.filter(x => x.dt_txt.includes('18:00:00'));
+    document.getElementById('wind').innerHTML = `${jsObject.wind.speed.toFixed(1)}mph`;
     sixpm.forEach(forecast => {
       let day = 0;
       let thedate = new Date(forecast.dt_txt);
-      document.querySelector(`#dayofweek${day + 1}`).innerHTML = thedate.getDay();
+      const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+      document.querySelector(`#dayofweek${day + 1}`).innerHTML = weekdays[thedate.getDay()];
+      document.querySelector(`#forecast${day + 1}`).innerHTML = forecast.main.temp;
       day++
-    });
+    }); 
   });
-  // 
-  //   const imagesrc = 'https://openweathermap.org/img/w/' + fiveDay.weather[0].icon + '.png';
-  //   const desc = fiveDay.weather[0].description; 
-  //   document.getElementById('forecast1')
-  //
