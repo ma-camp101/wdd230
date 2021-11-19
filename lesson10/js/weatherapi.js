451d0497487989f31cdc812a3c8a85a657d37ef3
ma-camp101 <cam20032@byui.edu>
2021-11-18 22:42:05 -0500

    changes

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,4 +1,4 @@
-const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=5b1c85008a1ef7a21d5e39aa1f79fd44"
+const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=5b1c85008a1ef7a21d5e39aa1f79fd44";
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
