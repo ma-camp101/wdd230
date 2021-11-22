da1e9d24f08c7eeff2bcb240c59543fb8838fad6
ma-camp101 <cam20032@byui.edu>
2021-11-22 18:58:03 -0500

    changes

---
+++ lesson11/js/upload.js
@@ -0,0 +1,157 @@
+// credit for this code goes to Supun Kavinda @ 'https://soshace.com/the-ultimate-guide-to-drag-and-drop-image-uploading-with-pure-javascript/' 
+
+let dropRegion = document.getElementById("drop-region"),
+	imagePreviewRegion = document.getElementById("image-preview");
+
+let fakeInput = document.createElement("input");
+fakeInput.type = "file";
+fakeInput.accept = "image/*";
+fakeInput.multiple = true;
+dropRegion.addEventListener('click', function() {
+	fakeInput.click();
+});
+
+fakeInput.addEventListener("change", function() {
+	let files = fakeInput.files;
+	handleFiles(files);
+});
+
+
+function preventDefault(e) {
+	e.preventDefault();
+  	e.stopPropagation();
+}
+
+dropRegion.addEventListener('dragenter', preventDefault, false)
+dropRegion.addEventListener('dragleave', preventDefault, false)
+dropRegion.addEventListener('dragover', preventDefault, false)
+dropRegion.addEventListener('drop', preventDefault, false)
+
+
+function handleDrop(e) {
+	let dt = e.dataTransfer,
+		files = dt.files;
+
+	if (files.length) {
+
+		handleFiles(files);
+		
+	} else {
+
+		// check for img
+		let html = dt.getData('text/html'),
+	        match = html && /\bsrc="?([^"\s]+)"?\s*/.exec(html),
+	        url = match && match[1];
+
+
+
+	    if (url) {
+	        uploadImageFromURL(url);
+	        return;
+	    }
+
+	}
+
+
+	function uploadImageFromURL(url) {
+		let img = new Image;
+        let c = document.createElement("canvas");
+        let ctx = c.getContext("2d");
+
+        img.onload = function() {
+            c.width = this.naturalWidth;     
+            c.height = this.naturalHeight;
+            ctx.drawImage(this, 0, 0);       
+            c.toBlob(function(blob) {       
+
+            	// call our main function
+                handleFiles( [blob] );
+
+            }, "image/png");
+        };
+        img.onerror = function() {
+            alert("Error in uploading");
+        }
+        img.crossOrigin = "";             
+        img.src = url;
+	}
+
+}
+
+dropRegion.addEventListener('drop', handleDrop, false);
+
+
+
+function handleFiles(files) {
+	for (let i = 0, len = files.length; i < len; i++) {
+		if (validateImage(files[i]))
+			previewAnduploadImage(files[i]);
+	}
+}
+
+function validateImage(image) {
+	let validTypes = ['image/jpeg', 'image/png', 'image/gif'];
+	if (validTypes.indexOf( image.type ) === -1) {
+		alert("Invalid File Type");
+		return false;
+	}
+
+	let maxSizeInBytes = 10e6; 
+	if (image.size > maxSizeInBytes) {
+		alert("File too large");
+		return false;
+	}
+
+	return true;
+
+}
+
+function previewAnduploadImage(image) {
+
+	let imgView = document.createElement("div");
+	imgView.className = "image-view";
+	imagePreviewRegion.appendChild(imgView);
+
+	let img = document.createElement("img");
+	imgView.appendChild(img);
+
+	let overlay = document.createElement("div");
+	overlay.className = "overlay";
+	imgView.appendChild(overlay);
+
+	let reader = new FileReader();
+	reader.onload = function(e) {
+		img.src = e.target.result;
+	}
+	reader.readAsDataURL(image);
+
+	let formData = new FormData();
+	formData.append('image', image);
+
+	let uploadLocation = 'https://api.imgbb.com/1/upload';
+	formData.append('key', 'bb63bee9d9846c8d5b7947bcdb4b3573');
+
+	let ajax = new XMLHttpRequest();
+	ajax.open("POST", uploadLocation, true);
+
+	ajax.onreadystatechange = function(e) {
+		if (ajax.readyState === 4) {
+			if (ajax.status === 200) {
+				// done!
+			} else {
+				// error!
+			}
+		}
+	}
+
+	ajax.upload.onprogress = function(e) {
+
+		let perc = (e.loaded / e.total * 100) || 100,
+			width = 100 - perc;
+
+		overlay.style.width = width;
+	}
+
+	ajax.send(formData);
+
+}
