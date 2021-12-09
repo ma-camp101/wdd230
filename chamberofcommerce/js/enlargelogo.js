6b6d7a087b22a665d59f7fcea8c78042ac26a666
ma-camp101 <cam20032@byui.edu>
2021-12-09 17:58:44 -0500

    changes

---
+++ chamberofcommerce/js/enlargelogo.js
@@ -0,0 +1,14 @@
+function enlargeLogo() {
+    let img = document.createElement("img");
+    if (media="(max-width:400px)") {
+        img.setAttribute('src', 'images/liberty_bell_in_quakertown_small_360px.jpg');
+        img.setAttribute('alt', 'Enlarged Version of Liberty house in Quakertown Pennsylvania with replica Liberty Bell');
+    } else if (media="(max-width:800px)") {
+        img.setAttribute('src', 'images/liberty_bell_in_quakertown_medium_700px.jpg');
+        img.setAttribute('alt', 'Enlarged Version of Liberty house in Quakertown Pennsylvania with replica Liberty Bell');
+    } else {
+        img.setAttribute('src', 'images/liberty_bell_in_quakertown_large_960px.jpg');
+        img.setAttribute('alt', 'Enlarged Version of Liberty house in Quakertown Pennsylvania with replica Liberty Bell');
+    }
+    document.querySelector('div.lightbox-target').appendChild(img);
+};
