c0910bebdb57cbf5bd646e0b36852bbd16abb359
ma-camp101 <cam20032@byui.edu>
2021-12-13 23:12:33 -0500

    changes

---
+++ chamberofcommerce/js/directory.js
@@ -0,0 +1,59 @@
+const businesses = 'json/businesses.json';
+fetch(businesses)
+
+  .then(function (response) {
+
+    if (!response.ok) {
+      throw Error(response.statusText);
+    } else {
+    return response.json();
+    }})
+
+  .then(function (jsonObject) {
+    let businesses = jsonObject['businesses'];
+    businesses.forEach((business) => {
+      
+      let card = document.createElement('section');
+      let h2 = document.createElement('h2');
+      let logo = document.createElement('img');
+      let photo = document.createElement('img');
+      let summary = document.createElement('p');
+      let website = document.createElement('a');
+      let address = document.createElement('p');
+      let address2 = document.createElement('p');
+      let phone = document.createElement('p');
+      let hours = document.createElement('p');
+
+
+      h2.innerHTML = `${business.name}`;
+      card.appendChild(h2);
+
+      logo.setAttribute('src', business.logo);
+      logo.setAttribute('alt', `${business.logoalt}`);
+      card.appendChild(logo);
+
+      photo.setAttribute('src', business.photo);
+      photo.setAttribute('alt', `${business.photoalt}`);
+      card.appendChild(photo);
+
+      summary.innerHTML = `${business.summary}`;
+      card.appendChild(summary);
+
+      website.setAttribute('href', business.website);
+      card.appendChild(website);
+
+      address.innerHTML = `${business.address}`;
+      card.appendChild(address);
+
+      address2.innerHTML = `${business.address2}`;
+      card.appendChild(address2);
+
+      phone.innerHTML = `${business.phone}`;
+      card.appendChild(phone);
+
+      hours.innerHTML = `${business.hours}`;
+      card.appendChild(hours);
+      
+    document.querySelector('div.cards').appendChild(card);
+    });
+  });
